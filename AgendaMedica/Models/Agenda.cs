60e9438c9b2eeb7d2c483c4d60f5aae7d046d0b7
ErickMerschbacher <erick.m.silva21@aluno.senai.br>
2025-09-12 09:21:06 -0300

    Adiciona suporte a entidades e funcionalidades CRUD
    Foram adicionadas as entidades `Especialidade`, `Medico`, `Paciente` e `Agenda` ao modelo de dados, com suas respectivas tabelas e relacionamentos configurados no `ApplicationDbContext`. Controladores e páginas de visualização foram criados para gerenciar essas entidades, implementando operações CRUD (Create, Read, Update, Delete).
    
    O layout foi atualizado para incluir links de navegação para as novas funcionalidades, e suporte à autenticação foi adicionado aos controladores para proteger as rotas. Migrações foram criadas para estruturar o banco de dados, e ajustes foram feitos para simplificar a entidade `Paciente`, removendo dependências desnecessárias.
    
    Essas alterações introduzem funcionalidades completas para o gerenciamento de especialidades, médicos, pacientes e agendas médicas no sistema.

---
+++ AgendaMedica/Models/Agenda.cs
@@ -0,0 +1,13 @@
+﻿namespace AgendaMedica.Models
+{
+    public class Agenda
+    {
+        public Guid AgendaId { get; set; }
+        public Guid MedicoId { get; set; } //FK
+        public Medico? Medico { get; set; }
+        public Guid PacienteId { get; set; } //FK
+        public Paciente? Paciente { get; set; }
+        public DateTime DataConsulta { get; set; }
+        public string? Status { get; set; }
+    }
+}
